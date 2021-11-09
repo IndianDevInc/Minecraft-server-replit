f151c0f5a4b07a8d14a497dc60b7a28e54ff1b05
Kartik OP <85619363+IndianDevInc@users.noreply.github.com>
2021-11-09 14:58:57 +0530

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,3 +1,3 @@
 echo "Running server..."
 echo "Subscribe :- https://www.youtube.com/c/CodingIndia"
-java -Xmx1G -Xms1G -jar paper-1.17.1.jar nogui
+java -Xmx1G -Xms1G -jar paper-1.17.1-377.jar nogui
